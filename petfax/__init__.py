6bb334f6fcd2c330ec49621cb72598aa153c393c
John Weilert <mattweilert@gmail.com>
2022-10-03 20:35:26 -0500

    Done

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,15 +1,20 @@
 # config
 from flask import Flask
-
-# factory
+from . import models
+from flask_migrate import Migrate
 
 
 def create_app():
     app = Flask(__name__)
 
-    # index route
+    # DB config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
     @app.route('/')
-    def index():
+    def hello():
         return 'Hello, PetFax!'
 
     # register pet blueprint
@@ -20,5 +25,5 @@ def create_app():
     from . import fact
     app.register_blueprint(fact.bp)
 
-    # return the app
+    # Return the app
     return app
